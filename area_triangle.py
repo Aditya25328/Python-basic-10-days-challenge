77d5e041a807b539e3be70991845cf6a21d984e8
Aditya25328 <aditya21401a@gmail.com>
2024-06-01 21:26:50 +0530

    Day 1

---
+++ area_triangle.py
@@ -0,0 +1,4 @@
+b=float(input("enter base:"))
+h=float(input("enter height:"))
+area=1/2(b*h)
+print("area is",area)
---
+++ celtofer.py
@@ -0,0 +1,3 @@
+cel=float(input("enter temperature in celsius :"))
+fer=(cel*9/5)+32
+print(f"{cel} celcius is {fer} in farhenite")
